c3f5b709bf2b448863b91b1809b8762a404b6e0f
Minh Tan <minhtan471100@gmail.com>
2022-08-12 15:57:52 +0700

    chapter 6

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "3.1.2"
 
+gem "bcrypt", "3.1.13"
 gem "bootstrap-sass", "3.4.1"
 gem "config"
 gem "jbuilder", "~> 2.7"
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save{email.downcase!}
+  validates(:name, presence: true, length: {maximum: Settings.validate.length_50})
+  validates(:email, presence: true, length: {maximum: Settings.validate.length_255},
+            format: {with: Settings.regex.email},
+            uniqueness: true)
+  validates(:password, presence: true, length: {minimum: Settings.validate.length_6})
+  has_secure_password
+end
